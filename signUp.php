19df841cf54218eb20815d44e101c3e4d60dd16d
MD Juman <mdjuman.developer.com>
2025-01-08 19:07:28 +0600

    March and fixed all files and folders everything is working

--- signUp.php
+++ signUp.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Home Page</title>
+   <title>Sign Up Page</title>
    <?php require("links.php") ?>
 </head>
 
@@ -16,14 +16,15 @@
 
    <!-- Main Content -->
    <main>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the Home Page of the webpage.</p>
+      <h1>Welcome to the Sign Up Page</h1>
+      <p>This is the Sign Up Page of the webpage.</p>
    </main>
-   
+
    <?php
    // Include the All Script CDN
     require("script.php");
     ?>
+
 </body>
 
 </html>
