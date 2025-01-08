1b66bfdefae491188b711677b4e3de38b5a2d1f7
Code By Rohim <arohim17577@gmail.com>
2025-01-08 20:39:32 +0600

    Product details and ralted product section

---
+++ productdetails.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exclusive</title>
+    <link
+        href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic"
+        rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+        rel="stylesheet" />
+    <script src="https://code.iconify.design/iconify-icon/2.2.0/iconify-icon.min.js"></script>
+    <link rel="stylesheet" href="./css/all.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/header.css" />
+    <link rel="stylesheet" href="./css/slick.css">
+    <link rel="stylesheet" href="./css/productdetails.css">
+    <link rel="stylesheet" href="./css/responsive.css" />
+</head>
+
+<body>
+    <?php
+    include ('header.php');
+    ?>
+<!------ Details Products Start Here -------->
+<section id="productDetails">
+        <div class="container">
+          <div class="row">
+            <!-------- Left Side -------->
+              <div class="slider-wrap d-flex justify-content-between col-12 col-lg-8" >
+                <div class="nav-cnt col-lg-3">
+                   
+                    <div class="slider-nav">
+                        
+                      <div class="product2">
+                        <img class="img-fluid" src="./img/controler1.svg" alt="product1">
+                      </div>
+                      <div class="product2">
+                        <img class="img-fluid" src="./img/controler2.svg" alt="product2">
+                      </div>
+                      <div class="product2">
+                        <img class="img-fluid" src="./img/controler3.svg" alt="product3">
+                      </div>
+                      <div class="product2">
+                        <img class="img-fluid" src="./img/controler4.svg" alt="product4">
+                      </div>
+                    </div>
+                    
+                </div>
+                <div class="for-cnt col-lg-9">
+                    <div class="slider-for">
+                      <div class="product1">
+                        <img class="img-fluid" src="./img/controler1.svg" alt="product1">
+                      </div>
+                      <div class="product1 ">
+                        <img class="img-fluid " src="./img/controler2.svg" alt="product2">
+                      </div>
+                      <div class="product1">
+                        <img class="img-fluid" src="./img/controler3.svg" alt="product3">
+                      </div>
+                      <div class="product1">
+                        <img class="img-fluid" src="./img/controler4.svg" alt="product4">
+                      </div>
+                    </div>
+                </div>
+              </div>           
+            <!-- Right Side -->
+            <div class="stock col-12 col-lg-4">
+              <h2 class="">Havic HV G-92 Gamepad</h2>
+              <span class="groupStar">
+                <i class="fa fa-star fa-star-checked orange"></i>
+                <i class="fa fa-star fa-star-checked orange"></i>
+                <i class="fa fa-star fa-star-checked orange"></i>
+                <i class="fa fa-star fa-star-checked orange"></i>
+                <i class="fa fa-star fa-star-checked white"></i>
+                <p>(150 Reviews) </p>
+                <span class="dot d-flex align-items-center"></span>
+                <h6 class="">In Stock</h6>
+              </span>
+              <div class="price">
+                  <h3>$192.00</h3>
+              </div>
+              
+              <!-- text -->
+              <p class="text-details">PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</p>
+              <hr>
+              <h5 class="colorChoose">Colours:
+                <div class="colorWrap">
+                  <span type="button" class="colorBlue"></span>
+                  <span type="button" class="colorRed"></span>
+                </div>
+              </h5>
+              <div class="size">Size:
+                <div class="sizeWrap">
+                  <span type="button">XS</span>
+                  <span type="button">S</span>
+                  <span type="button">M</span>
+                  <span type="button">L</span>
+                  <span type="button">XL</span>
+                </div>
+              </div>
+              <!-- quantity -->
+              <div class="cartItemQuantity">
+                <div class="quantity">
+                  <button>-</button>
+                  <input type="text" value="2">
+                  <button>+</button>
+                </div>
+                <div class="cartAddBtn">
+                    <button>Buy Now</button>
+                </div>
+                <a class="heart" href="#"><i class="fa-regular fa-heart"></i></a>
+              </div>
+             
+              <!-- Delivery-->
+               <div class="cardDelivery">
+                   <div class="free">
+                    <img src="./img/icon-delivery.png" alt="">
+                    <div>
+                        <h5>FreeDelivery</h5>
+                        <p>Enter your postal code for Delivery Availability</p>
+                    </div>
+                   </div>
+                   <hr>
+                   <div class="free">
+                    <img src="./img/Icon-return.png" alt="">
+                    <div>
+                        <h5>Return Delivery</h5>
+                        <p>Free 30 Days Delivery Returns. Details</p>
+                    </div>
+                   </div>
+               </div>
+          </div>
+          </div>
+        </div>
+</section>
+<!------ Details Products End Here -------->
+
+ <!-------- Related Items Start Here-------->
+    <section id="relateItem">
+      <div class="container">
+        <h2><div></div>Related Item</h2>
+        <div class="productWrap">
+          <!-- product 1 -->
+                <div class="product">
+                  <div class="productImg">
+                    <label for="">-40%</label>
+                      <img class="img-fluid" src="./img/related (2).png" alt="">
+
+                      <div class="overlay">
+                          <div>
+                            <i class="fa-regular fa-heart"></i>
+                            <i class="fa-regular fa-eye"></i>
+                          </div>
+                      <button class="btn btn-primary">Add To Cart</button>
+                    </div>
+                  </div>
+                  <div class="productText">
+                    <h5>HAVIT HV-G92 Gamepad</h5>
+                    <h6>$120 <span>$160</span></h6>
+                    <span class="rating">
+                        <div>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                        </div>
+                      <p>(88)</p>
+                    </span>
+                  </div>  
+                </div>
+          <!-- product 2 -->
+                <div class="product">
+                  <div class="productImg">
+                    <label for="">-35%</label>
+                      <img class="img-fluid" src="./img/related (3).png" alt="">
+
+                      <div class="overlay">
+                          <div>
+                            <i class="fa-regular fa-heart"></i>
+                            <i class="fa-regular fa-eye"></i>
+                          </div>
+                      <button class="btn btn-primary">Add To Cart</button>
+                    </div>
+                  </div>
+                  <div class="productText">
+                    <h5>AK-900 Wired Keyboard</h5>
+                    <h6>$960 <span>$1160</span></h6>
+                    <span class="rating">
+                        <div>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked white"></i>
+                        </div>
+                      <p>(75)</p>
+                    </span>
+                  </div>  
+                </div>
+          <!-- product 3 -->
+                <div class="product">
+                  <div class="productImg">
+                    <label for="">-30%</label>
+                      <img class="img-fluid" src="./img/related (4).png" alt="">
+
+                      <div class="overlay">
+                          <div>
+                            <i class="fa-regular fa-heart"></i>
+                            <i class="fa-regular fa-eye"></i>
+                          </div>
+                      <button class="btn btn-primary">Add To Cart</button>
+                    </div>
+                  </div>
+                  <div class="productText">
+                    <h5>IPS LCD Gaming Monitor</h5>
+                    <h6>$370 <span>$400</span></h6>
+                    <span class="rating">
+                        <div>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked white"></i>
+                        </div>
+                      <p>(88)</p>
+                    </span>
+                  </div>  
+                </div>
+          <!-- product 4 -->
+                <div class="product">
+                  <div class="productImg">
+                    <!-- <label for="">-40%</label> -->
+                      <img class="img-fluid" src="./img/related (1).png" alt="">
+
+                      <div class="overlay">
+                          <div>
+                            <i class="fa-regular fa-heart"></i>
+                            <i class="fa-regular fa-eye"></i>
+                          </div>
+                      <button class="btn btn-primary">Add To Cart</button>
+                    </div>
+                  </div>
+                  <div class="productText">
+                    <h5>RGB liquid CPU Cooler</h5>
+                    <h6>$160 <span>$170</span></h6>
+                    <span class="rating">
+                        <div>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                          <i class="fa fa-star fa-star-checked orange"></i>
+                        </div>
+                      <p>(65)</p>
+                    </span>
+                  </div>  
+                </div>
+        </div>
+      </div>
+    </section>
+   <!------------- Related Items End Here---------->
+   <?php
+    include ('foote.php');
+    ?>
+</body>
+<script src="./js/bootstrap.bundle.min.js"></script>
+<script src="./js/jquery-3.7.1.min.js"></script>
+<script src="./js/slick.min.js"></script>
+<script src="./js/app.js"></script>
+</body>
+
+</html>
